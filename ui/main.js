40dc72df961aef30906dea3f8b2903f4ce5a16cc
nikmul19 <nikmul19@gmail.com>
2017-07-16 20:16:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var img=document.getElementById('me');
 var marginLeft=0;
 function moveLeft()
 {
-    if(img.style.marginLeft === '450px')break;
+  //  if(img.style.marginLeft === '450px')break;
     marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft + 'px';
 }
