fdc34cdaa4594cb2df0c9e2a3b1d320893e1de9a
nikmul19 <nikmul19@gmail.com>
2017-07-17 22:39:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var config={
     database:'nikmul19',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD};
+    password:process.env.DB_PASSWORD
+    
+};
     
 
 var app = express();
@@ -16,23 +18,25 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool=new Pool(config);
 
 app.get('/test-db', function (req, res) {
 
-    pool.query('select * from test',function(err,result)
-    {   if(err)
+    pool.query('SELECT * FROM test',function(err,result)
+    { 
+        if(err)
     {
         res.status(500).send(err.toString());
     }
+        
         else {
     res.send(JSON.stringify(result))
-}
-});
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
