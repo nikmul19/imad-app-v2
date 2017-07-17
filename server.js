ffc51650906aa5b3dbe1204a2349f3238bc320c1
nikmul19 <nikmul19@gmail.com>
2017-07-17 23:13:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool=new Pool(config);
 
 app.get('/test-db', function (req, res) {
 
